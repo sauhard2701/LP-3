830948f8b039d3fce615e7c96bd41c854b9d61d0
Sauhard Saini <sauhardsaini2701@gmail.com>
2022-11-23 11:46:03 +0530

    Update ML3.py

--- ML3.py
+++ ML3.py
@@ -28,7 +28,7 @@ y
 from sklearn.model_selection import train_test_split
 #-----------------------------#
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 1)
-x_train.shape, x_test.shape
+
 #-----------------------------#
 print("The shape of X_train is:",x_train.shape)
 print("The shape of X_test is:",x_test.shape)
